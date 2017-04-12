9a89fa1d1fae43b8306fbe3d0732680e7b8b27b0
lHatePeople <sergeybednyuk@gmail.com>
2017-04-12 13:37:40 +0200

    Create servise

---
+++ HomeTask1/ServiceDiskInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDiskInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Host";
+
+            Uri address = new Uri("http://localhost:4000/IDiskInfo");//Adress (A)
+            BasicHttpBinding binding = new BasicHttpBinding();//Binding (B)
+            Type contract = typeof(IDiskInfo);//Contract (C)
+
+            //create Host
+            ServiceHost host = new ServiceHost(typeof(Service));
+            //create Endpoint
+            host.AddServiceEndpoint(contract, binding, address);
+
+            host.Open();
+            Console.WriteLine("Host works");
+            Console.ReadKey();
+            Console.WriteLine("Host will close");
+        }      
+    }
+}
---
+++ HomeTask1/ServiceDiskInfo/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ServiceDiskInfo
+{
+    class Service:IDiskInfo
+    {
+        public string[] GetDiskInfo(string path)
+        {
+            return Directory.GetFileSystemEntries(@path);
+        }
+    }
+}
